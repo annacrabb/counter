3a941e3c6af1a3d0bdc65541bdc080061defa011
Anna Crabb <annacrabb92@gmail.com>
2024-05-27 15:49:46 -0400

    built

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import statements here - import css, 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    count: 0
+  };
+  // bind clicks
+this.increase = this.increase.bind(this);
+this.decrease = this.decrease.bind(this);
+this.reset = this.reset.bind(this);
 }
+
+state = {
+  count: 0
+}
+
+// do I need to put state this in a constructor? when to use either?
+
+// functions for increase, decrease, and reset
+increase() {
+  this.setState({ 
+    count: this.state.count + 1
+  });
+}
+
+decrease() {
+  this.setState({ 
+    count: this.state.count - 1
+  });
+}
+
+reset()  {
+  this.setState({ 
+    count: 0
+  }); // can this just be count: 0
+  
+}
+
+  render() {
+    return (
+      <div>
+      <h1>Count sheep with me!</h1>
+      <h3>We currently have {this.state.count} sheep.</h3>
+      <button onClick={this.increase}>More sheep!</button>
+      <button onClick={this.decrease}>Fewer sheep!</button>
+      <button onClick={this.reset}>Reset sheep</button>
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
